3f13a7e62dfa9e0a1ccfab59d921d1599eb0f65a
daidai <82143372@qq.com>
2018-08-26 13:23:25 +0800

    忘记了一周的提交

---
+++ 常用代码收集/初始化代码/各种js初始化代码.js
@@ -0,0 +1,15 @@
+//自定义选区DOM元素$简写方法(ES6)
+const $ = document.querySelectorAll.bind(document);
+
+//jquery的初始化
+$(function(){
+	//初始化内容
+})
+
+$(document).ready(function(){
+	//初始化内容
+})
+
+jQuery(function($){
+	//初始化内容
+})
