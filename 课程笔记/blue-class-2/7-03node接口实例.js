ff36cae9a0474763a02943d671cb249b76206730
daixiyu <82143372@qq.com>
2018-09-05 17:21:50 +0800

    2018/9/5

--- 课程笔记/blue-class-2/7-03node接口实例.js
+++ 课程笔记/blue-class-2/7-03node接口实例.js
@@ -2,11 +2,14 @@ const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 const fs = require('fs');
+let users = {};
 
 let server = http.createServer((req, res)=>{
 	if(req.url!=='/favicon.ico'){
+		// res.setHeader('Access-Control-Allow-Origin', '*');
 		let { pathname, query: getData }=url.parse(req.url, true);
 		let str = '';
+		
 		req.on('data',(data)=>{
 			str+=data;
 		})
@@ -15,9 +18,21 @@ let server = http.createServer((req, res)=>{
 			console.log(getData, postData);
 
 			switch(pathname){
-				case '/reg':
-					break;
 				case '/login':
+					let { user, pass }=getData;
+
+					if(!user){
+						res.write('{"err": 1, "msg": "user master be write"}');
+					}else if(!pass){
+						res.write('{"err": 1, "msg": "pass master be write"}');
+					}else{
+						res.write('{"err": 0, "msg": "OK"}');
+						users[user]=pass;
+						console.log(users);
+					}
+					res.end();
+					break;
+				case '/reg':
 					break;
 				default:
 					fs.readFile(`data${pathname}`,(err, data)=>{
@@ -27,12 +42,14 @@ let server = http.createServer((req, res)=>{
 						}else{
 							res.write(data);
 						}
+						res.end();
 					})
 			}
+
 		})
 
 		// console.log(getData, postData);   此条程序被先于req.on('end')执行了,postData为空,所以后续操作写在req.on('end')内
 	}
-	res.end();
+	
 })
 server.listen(8050);
