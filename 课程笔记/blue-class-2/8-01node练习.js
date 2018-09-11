b0a6261b63708fe8142541893f509a13a63842a3
daidai <82143372@qq.com>
2018-09-11 22:45:29 +0800

    2018/9/11 n

--- 课程笔记/blue-class-2/8-01node练习.js
+++ 课程笔记/blue-class-2/8-01node练习.js
@@ -15,15 +15,26 @@ let server = http.createServer((req, res)=>{
 					let boundary = req.headers['content-type'].split('=')[1];
 					let arr1 = str.split(boundary).slice(1,-1);
 					let arr2 = arr1.map(item=>item.slice(2,-4));
+					let fileIndex = '';
+					let keyValue = {};
 					arr2.forEach(item=>{
 						let n=item.indexOf('\r\n\r\n');
-						let index = item
+						console.log('======='+item.slice(0,n).indexOf('\r\n'));
+						if(item.slice(0,n).indexOf('\r\n')!=-1){
+							fileIndex = item.slice(n+4);
+						}else{
+							let key = item.slice(0,n).split('; ')[1].split('=')[1];
+							let value = item.slice(n+4);
+							keyValue[key]=value;
+						}
+						
 					});
 
 
-					console.log(arr1);
+					// console.log(arr1);
 					console.log(arr2);
-					console.log(indexArr);
+					console.log(fileIndex);
+					console.log(keyValue);
 					res.end();
 					break;
 				default:
