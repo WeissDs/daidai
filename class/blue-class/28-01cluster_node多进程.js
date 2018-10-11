a749408e811cd5c45582b1ac2b680dd8f1a56ea3
daixiyu <82143372@qq.com>
2018-10-11 17:32:44 +0800

    2018/10/11

--- class/blue-class/28-01cluster_node多进程.js
+++ class/blue-class/28-01cluster_node多进程.js
@@ -17,3 +17,15 @@ if(cluster.isMaster){
 }
 
 // console.log('多进程');
+
+/*
+	多进程和多线程：
+		多进程——重：独立储存空间   安全，性能相对较低   	PHP
+		多线程——轻：同一进程只内的线程共享空间   不安全，性能高		JAVA
+
+		JS: 单进程，非阻塞
+			前端：WebWorker		实现多进程
+			后端：Node.js——cluster		实现多进程
+
+	node框架
+*/
