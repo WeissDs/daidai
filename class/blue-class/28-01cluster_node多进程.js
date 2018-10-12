f010d0bbdea69ac9ec417ec9528a68263a9ce847
daixiyu <82143372@qq.com>
2018-10-12 17:31:36 +0800

    2018/10/12

--- class/blue-class/28-01cluster_node多进程.js
+++ class/blue-class/28-01cluster_node多进程.js
@@ -1,5 +1,6 @@
 const cluster = require('cluster');
-const os = require('os');
+const os = require('os');   //operating system： 操作系统
+const process = require('process');
 
 // if(cluster.isMaster){
 // 	cluster.fork();
@@ -13,19 +14,26 @@ if(cluster.isMaster){
 	}
 	console.log('主进程');
 }else{
-	console.log('子进程');
-}
+	console.log('子进程'+process.pid)
+};
+	
 
 // console.log('多进程');
 
 /*
-	多进程和多线程：
+	1. 多进程和多线程：
 		多进程——重：独立储存空间   安全，性能相对较低   	PHP
 		多线程——轻：同一进程只内的线程共享空间   不安全，性能高		JAVA
 
 		JS: 单进程，非阻塞
 			前端：WebWorker		实现多进程
 			后端：Node.js——cluster		实现多进程
+		
+		普通程序无法创建多进程，只有系统有创建进程的权限
 
-	node框架
+			1. 1 那么js是怎么创建多进程？
+				通过内部分裂（fork）
+
+	2. node框架
+		Express、KOA、egg、thinkjs
 */
