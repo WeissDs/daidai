4ad659bae1d93abd56f8541ec5dd738d412fbda1
daixiyu <82143372@qq.com>
2018-10-30 17:37:37 +0800

    2018/10/30

--- class/blue-class/28-03~29Express/29_04 express其他用法.js
+++ class/blue-class/28-03~29Express/29_04 express其他用法.js
@@ -20,51 +20,6 @@ server.use(multerObj.fields(接收特定数据)或者.any(接收任何数据).no
 req.files
 
 
-
-cookie、session：
-
-1. cookie——浏览器存数据，在请求服务器的时候，会被带到请求里（服务器端和浏览器端都可以操作）
-	缺点：容量有限（4k）（cookie是在请求头里传输的）、不安全（用户可以随意篡改）
-
-在一个浏览器第一次请求一个服务器的时候会发送一个空的cookie[] , 服务器会返回一个cookie[sess_ID] 
-就是一串uuid码， 用来标识该用户，等到下次这个浏览器再访问该服务器的时候，会将这个cookie[sess_ID]
-带过去。
-
-	风险：如果sess_ID泄露了  ———— 就叫做session劫持
-
-	
-	需要先添加cookie-parser：
-	[1].设置：res.cookie(name, value, options{
-		domain:
-		expires: date
-		maxAge: int
-		path
-		secure: true
-		signed: true
-	})
-	[2].读取：res.cookies
-	[3].安全（防篡改）：
-
-2. session——只存在服务器端
-	优点：容量无限（服务器容量多大就能多大）、安全（用户无法看见和篡改）
-	session是基于cookie的
-
-	[1].设置：
-	[2].读取：
-	[3].安全（防篡改）：
-
-
-
-
-
-
-路由：
-
-
-
-渲染（服务端渲染，客户端渲染）：
-
-
 */ 
 
 const express = require('express');
@@ -163,22 +118,4 @@ server.post('/upload', (req, res, next)=>{
 	}
 
 })
-
-
-
-//cookie 和 session-----------------------------------------------------------------------------
-
-//下载 cookie-parser 和 cookie-session  -D
-
-const cookieParser = require('cookie-parser');
-
-server.use(cookieParser('dfiiii')) //值可以是数组，也可以是字符串
-
-server.get('/cookie', (req, res, next)=>{
-	console.log(req.cookies);
-	console.log(req.signedCookies);
-	res.cookie('dd', 'ofisoneonnennn', {signed: true});
-	res.end();
-})
-
 
